7386346fe2c780eb80f45685d06a38e9b90ad108
pannet1 <prog@ecomsense.in>
2026-04-18 11:17:20 +0530

    Add cron.py script to handle start/stop from cron

---
+++ factory/cron.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+import subprocess
+import sys
+
+action = sys.argv[1] if len(sys.argv) > 1 else "start"
+
+if action == "start":
+    subprocess.run(["/usr/bin/sudo", "systemctl", "start", "uma-scalper.service"])
+elif action == "stop":
+    subprocess.run(["/usr/bin/sudo", "systemctl", "stop", "uma-scalper.service"])
+elif action == "restart":
+    subprocess.run(["/usr/bin/sudo", "systemctl", "restart", "uma-scalper.service"])
