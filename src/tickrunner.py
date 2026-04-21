8e042040ba99464fcb7a903c3c61eec030fd5048
pannet1 <prog@ecomsense.in>
2026-04-21 14:19:22 +0530

    Debug: More LTP logging

--- src/tickrunner.py
+++ src/tickrunner.py
@@ -127,7 +127,8 @@ class TickRunner:
                 self.exit_id = ""
             elif item and item.get("status", None) == "OPEN":
                 ltp = self.ltps.get(self.symbol)
-                logging.info(f"exit_trade: symbol={self.symbol}, ltp_keys={list(self.ltps.keys())}, ltp={ltp}")
+                ws_ltp_keys = list(self.ws.ltp.keys())
+                logging.info(f"exit_trade: symbol={self.symbol} in tokens_nearest={self.symbol in self.tokens_nearest} in ltps={self.symbol in self.ltps}, ws_ltp_keys={ws_ltp_keys[:3]}..., ltp={ltp}")
                 if ltp and (ltp > self.target_price or ltp < self.exit_price):
                     logging.info(f"Target reached for {self.exit_id}, modifying to market")
                     kwargs = dict(
