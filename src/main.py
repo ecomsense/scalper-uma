94096b9b3daf56e4949795f55101d91eb3f5c74b
pannet1 <prog@ecomsense.in>
2026-04-23 10:55:49 +0530

    sse: always update on order change

--- src/main.py
+++ src/main.py
@@ -572,8 +572,7 @@ async def stream_all_orders(request: Request) -> EventSourceResponse:
                         print(order_msg, "/n", "ORDER UPDATE FROM WEBSOCKET")
                         yield {"event": "order_msg", "data": msg_str}
                 orders_cache = Helper.orders()
-                if orders_cache and len(orders_cache) > 1:
-                    yield {"event": "order_update", "data": json.dumps(orders_cache)}
+                yield {"event": "order_update", "data": json.dumps(orders_cache)}
 
             except Exception as e:
                 print("Order SSE error:", e)
